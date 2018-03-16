a64886a53618cc013f9caba6f506db8a7de451a0
hadaeghimohsen <hadaeghimohsen@yahoo.com>
2018-03-17 00:57:03 +0330

    Update

---
+++ Stored Procedures/Global.SavePosDevice.sql
@@ -0,0 +1,85 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [Global].[SavePosDevice]
+	-- Add the parameters for the stored procedure here
+	@X XML
+AS
+BEGIN   
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+   BEGIN TRY
+   BEGIN TRAN SavePosDevice_Tran
+      DECLARE @Psid BIGINT
+             ,@BankType VARCHAR(3)
+             ,@BnkbCode VARCHAR(10)
+             ,@BnkaAcntNumb VARCHAR(15)
+             ,@ShbaCode VARCHAR(50)
+             ,@PosDesc NVARCHAR(250)
+             ,@PosStat VARCHAR(3)
+             ,@PosDflt VARCHAR(3)
+             ,@PosCnctType VARCHAR(3)
+             ,@IPAdrs VARCHAR(15)
+             ,@CommPort VARCHAR(15)
+             ,@BandRate INT
+             ,@PrntSale NVARCHAR(250)
+             ,@PrntCust NVARCHAR(250);
+      
+      SELECT @Psid = @X.query('Pos').value('(Pos/@psid)[1]', 'BIGINT')
+            ,@BankType = @X.query('Pos').value('(Pos/@banktype)[1]', 'VARCHAR(3)')
+            ,@BnkbCode = @X.query('Pos').value('(Pos/@bnkbcode)[1]', 'VARCHAR(10)')
+            ,@BnkaAcntNumb = @X.query('Pos').value('(Pos/@bnkaacntnumb)[1]', 'VARCHAR(15)')
+            ,@ShbaCode = @X.query('Pos').value('(Pos/@shbacode)[1]', 'VARCHAR(50)')
+            ,@PosDesc = @X.query('Pos').value('(Pos/@posdesc)[1]', 'NVARCHAR(250)')
+            ,@PosStat = @X.query('Pos').value('(Pos/@posstat)[1]', 'VARCHAR(3)')
+            ,@PosDflt = @X.query('Pos').value('(Pos/@posdflt)[1]', 'VARCHAR(3)')
+            ,@PosCnctType = @X.query('Pos').value('(Pos/@poscncttype)[1]', 'VARCHAR(3)')
+            ,@IPAdrs = @X.query('Pos').value('(Pos/@ipadrs)[1]', 'VARCHAR(15)')
+            ,@CommPort = @X.query('Pos').value('(Pos/@commport)[1]', 'VARCHAR(15)')
+            ,@BandRate = @X.query('Pos').value('(Pos/@bandrate)[1]', 'INT')
+            ,@PrntSale = @X.query('Pos').value('(Pos/@prntsale)[1]', 'NVARCHAR(250)')
+            ,@PrntCust = @X.query('Pos').value('(Pos/@prntcust)[1]', 'NVARCHAR(250)');
+      
+      MERGE Global.Pos_Device T
+      USING (SELECT @Psid AS PSID) S
+      ON (T.PSID = S.PSID)
+      WHEN NOT MATCHED THEN 
+         INSERT (PSID, BANK_TYPE, BNKB_CODE, BNKA_ACNT_NUMB, SHBA_CODE, POS_DESC, POS_STAT, POS_DFLT, SEND_AMNT_EDIT, SEND_DATA_ON_DEVC, FILL_RSLT_DATA, POS_CNCT_TYPE, IP_ADRS, COMM_PORT, BAND_RATE, PRNT_SALE, PRNT_CUST)
+         VALUES (S.PSID, @BankType, @BnkbCode, @BnkaAcntNumb, @ShbaCode, @PosDesc, @PosStat, @PosDflt, '001', '002', '002', @PosCnctType, @IPAdrs, @CommPort, @BandRate, @PrntSale, @PrntCust)
+      WHEN MATCHED THEN
+         UPDATE SET
+            T.BANK_TYPE = @BankType
+           ,T.BNKB_CODE = @BnkbCode
+           ,T.BNKA_ACNT_NUMB = @BnkaAcntNumb
+           ,T.SHBA_CODE = @ShbaCode
+           ,T.POS_DESC = @PosDesc
+           ,T.POS_STAT = @PosStat
+           ,T.POS_DFLT = @PosDflt
+           ,T.POS_CNCT_TYPE = @PosCnctType
+           ,T.IP_ADRS = @IPAdrs
+           ,T.COMM_PORT = @CommPort
+           ,T.BAND_RATE = @BandRate
+           ,T.PRNT_SALE = @PrntSale
+           ,T.PRNT_CUST = @PrntCust;
+      
+   COMMIT TRAN SavePosDevice_Tran;   
+   END TRY
+   BEGIN CATCH
+      DECLARE @ErorMesg NVARCHAR(MAX);
+      SET @ErorMesg = ERROR_MESSAGE();
+      RAISERROR ( @ErorMesg, -- Message text.
+               16, -- Severity.
+               1 -- State.
+               );
+      ROLLBACK TRAN SavePosDevice_Tran;
+   END CATCH   
+END
+GO
---
+++ Tables/Global.Pos_Device.sql
@@ -0,0 +1,101 @@
+CREATE TABLE [Global].[Pos_Device]
+(
+[PSID] [bigint] NOT NULL,
+[BANK_TYPE] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[BNKB_CODE] [varchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[BNKA_ACNT_NUMB] [varchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SHBA_CODE] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[POS_DESC] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[POS_STAT] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_POS_STAT] DEFAULT ('002'),
+[POS_DFLT] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_POS_DFLT] DEFAULT ('001'),
+[SEND_AMNT_EDIT] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_SEND_AMNT_EDIT] DEFAULT ('002'),
+[SEND_DATA_ON_DEVC] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_SEND_DATA_ON_DEVC] DEFAULT ('001'),
+[FILL_RSLT_DATA] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_FILL_RSLT_DATA] DEFAULT ('001'),
+[POS_CNCT_TYPE] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_POS_CNCT_TYPE] DEFAULT ('000'),
+[IP_ADRS] [varchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[COMM_PORT] [varchar] (15) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Pos_Subsystem_COMM_PORT] DEFAULT ('COM1'),
+[BAND_RATE] [int] NULL CONSTRAINT [DF_Pos_Subsystem_BAND_RATE] DEFAULT ((9600)),
+[PRNT_SALE] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PRNT_CUST] [nvarchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CRET_BY] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CRET_DATE] [date] NULL,
+[MDFY_BY] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[MDFY_DATE] [date] NULL
+) ON [BLOB]
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [Global].[CG$AINS_POS]
+   ON  [Global].[Pos_Device]
+   AFTER INSERT
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+   MERGE Global.Pos_Device T
+   USING (SELECT * FROM Inserted) S
+   ON (T.PSID = S.PSID)
+   WHEN MATCHED THEN
+      UPDATE SET
+         T.CRET_BY = UPPER(SUSER_NAME())
+        ,T.CRET_DATE = GETDATE()
+        ,T.PSID = dbo.GetNewIdentity();
+
+END
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [Global].[CG$AUPD_POS]
+   ON  [Global].[Pos_Device]
+   AFTER UPDATE
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+   MERGE Global.Pos_Device T
+   USING (SELECT * FROM Inserted) S
+   ON (T.PSID = S.PSID)
+   WHEN MATCHED THEN
+      UPDATE SET
+         T.MDFY_BY = UPPER(SUSER_NAME())
+        ,T.MDFY_DATE = GETDATE();
+END
+GO
+ALTER TABLE [Global].[Pos_Device] ADD CONSTRAINT [PK_POS] PRIMARY KEY CLUSTERED  ([PSID]) ON [BLOB]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'میزان باند ', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'BAND_RATE'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'پورت سریال', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'COMM_PORT'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'اطلاعات دریافتی در مورد پرداخت هزینه از دستگاه ذخیره گردد', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'FILL_RSLT_DATA'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'IP', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'IP_ADRS'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'روش اتصال به دستگاه', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'POS_CNCT_TYPE'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'دستگاه پیش فرض', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'POS_DFLT'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'وضعیت فعال بودن دستگاه', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'POS_STAT'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'آیا مبلغ ارسالی قابل ویرایش می باشد', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'SEND_AMNT_EDIT'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'داده ها به روی دستگاه ارسال شود', 'SCHEMA', N'Global', 'TABLE', N'Pos_Device', 'COLUMN', N'SEND_DATA_ON_DEVC'
+GO
