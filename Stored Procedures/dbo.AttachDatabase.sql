bbfb38be4e68903710d3bb567332e6d134600281
Mohsen Hadaeghi <hadaeghimohsen@yahoo.com>
2018-12-29 15:27:35 +0330

    Update

---
+++ Stored Procedures/dbo.AttachDatabase.sql
@@ -0,0 +1,35 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[AttachDatabase]
+	@X XML
+AS
+BEGIN
+  /*
+   '<Request Rqtp_Code="ManualSaveHostInfo">
+      <Database>iProject</Database>
+      <Dbms>SqlServer</Dbms>
+      <User>scott</User>
+      <Computer name="DESKTOP-LB0GKTR" 
+                ip="192.168.158.1" 
+                mac="00:50:56:C0:00:01" 
+                cpu="BFEBFBFF000206A7" />
+    </Request>'
+   */
+   DECLARE  @ComputerName VARCHAR(50)
+           ,@Cpu      VARCHAR(30);
+   
+   SELECT @ComputerName = @X.query('//Computer').value('(Computer/@name)[1]', 'VARCHAR(50)')
+         ,@Cpu = @X.query('//Computer').value('(Computer/@cpu)[1]', 'VARCHAR(30)');
+         
+   UPDATE Report.DataSource 
+      SET IPAddress = @ComputerName
+     WHERE DatabaseServer = 1;
+END
+GO
